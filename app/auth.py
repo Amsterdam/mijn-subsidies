ccbf6937fc6f659b604c7de06853a5890ea59733
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-04-14 10:43:28 +0200

    Update auth

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest.mock import patch
 from flask_httpauth import HTTPTokenAuth
 import jwt
 
@@ -10,9 +11,9 @@ AuthException = jwt.PyJWTError
 PROFILE_TYPE_PRIVATE = "private"
 PROFILE_TYPE_COMMERCIAL = "commercial"
 
-OIDC_CLIENT_SECRET = os.getenv("OIDC_CLIENT_SECRET", "")
 OIDC_CLIENT_ID_DIGID = os.getenv("OIDC_CLIENT_ID_DIGID", "digid")
 OIDC_CLIENT_ID_EHERKENNING = os.getenv("OIDC_CLIENT_ID_EHERKENNING", "eherkenning")
+OIDC_JWKS_URL = os.getenv("OIDC_JWKS_URL", "")
 
 TOKEN_ID_ATTRIBUTE_COMMERCIAL = "urn:etoegang:1.9:EntityConcernedID:KvKnr"
 TOKEN_ID_ATTRIBUTE_PRIVATE = "sub"
@@ -53,18 +54,28 @@ def get_profile_id(token_data):
     return token_data[id_attribute]
 
 
-def get_user_profile_from_token(token):
+def get_verified_token_data(token):
+    jwks_client = jwt.PyJWKClient(OIDC_JWKS_URL)
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+
     audience = [
         get_client_id(PROFILE_TYPE_PRIVATE),
         get_client_id(PROFILE_TYPE_COMMERCIAL),
     ]
+
     token_data = jwt.decode(
         token,
-        OIDC_CLIENT_SECRET,
+        signing_key.key,
+        algorithms=["RS256"],
         audience=audience,
-        algorithms=["HS256"],
-        options={"verify_signature": True, "verify_exp": True, "verify_iat": True},
+        options={"complete": True},
     )
+
+    return token_data
+
+
+def get_user_profile_from_token(token):
+    token_data = get_verified_token_data(token)
 
     profile_type = get_profile_type(token_data)
     profile_id = get_profile_id(token_data)
@@ -88,6 +99,34 @@ class FlaskServerTestCase(unittest.TestCase):
     app = None
     client = None
 
+    rsa_private_key_test = {
+        "p": "8b-T1GJux6AGYWz1FLaXdTVkXsVQ2_oNFMs-gJBRXMpDT_1g3LlrjtEd_Y2-HuaDbEAoS8ccGlC9IIjbYcunQBqD1whl3tiGFswzDk2DUaJjXZnPAjYHWUHa1cl3tkDEo9uzWJ0h201QH7bG0Ls2Jl1IPOtSzPcNHBO0iWg_WH0",
+        "kty": "RSA",
+        "q": "3GtC1fHI297LqVHGN9btnf5nt7pT_TVWltYxio3DJvrNsAHiAHmwr87FNheSLcaBgUgqGYcGnQrvnW4Ly_c5Sb_xEMwigp5TcpjPYjHZGv5ML5Rf8yEZJAjiFJ6RuUWRHOZ0qRJSnFuVDdj1xfH4dUkZGfJ2vl9DMm6mRhGk6As",
+        "d": "mYegF_YD30wsYPrk241n7vsEk7tnCqqFPd25_5XRwHeo33qSiQMgDKvpHjthoWMCMmY_e9V_af-Ht_eX6uM0T7mMrQCpAvjeOrcRd1vMuMxVoMOTmVrn_wZNEFaYTs4zTmy3-fYjQiB1le1kOGO6t03FXeQFTWgJQTTVOCrHAIILrOSj0HqtQomzsw9J7MLXd2eRuKDZydRSbJEhPg3NUzHeJjbuKJg6aVlj-gSaQ1s79vteIjm3pwItAkEsSP5R4LCrlxPPdIW4ghemGwi2jIfJhxzW7v3Q0sI6MYZ10FwkiUh9W1IUbUADQIT7Jf7EZ1yt_u8s7c9dvJ-NotHi4Q",
+        "e": "AQAB",
+        "use": "sig",
+        "kid": "8YN3pNDUUyho-tQB25afq8DKCrxt2V-bS6W9gRk0cgk",
+        "qi": "fAfkX1oqy_U-vU_eaCgEHYvZZxS7r1pSZpqipFitJdHayqlZEVwddmQZZ30IX3tHk3NRfjm6zy6FCXrVXVleAOkPyJpPXVsK_GiUufh28u5hPncs3KaFU0tTQ373Vd7IgOF7IhshyImR6UAXQiLSPLaEFQdte5DRL4kMkgwYHF8",
+        "dp": "EyIpjh64S95zgtR_1ULaW_F83y9YxgBVdrbbXIuPlPuBNlyEhRO72pLcf8vvJzzxW-j8B3tb0w1e2qtaSbQ3qZAvrR5CCdAzVKyWweQKp7Rljuv0gWVLUZovusn2Spt3tMxXtoTBQD0vQUNTGwQmNgUeCYxKgmRvSjCZEmMI2HU",
+        "alg": "RS256",
+        "dq": "2xIAK4NTjrOw12hfCcCkChOAIisertsEZIYeVwbunx9Gr1gvtyk7YoCvoUNsFfLlZAjFTvnUqODlpiJptx7P4WzTu04oPon9hjg6Ze4FSb7VGbTuaEbNJfNuP_AaBXoO8BpceG2tjZm4Wzr3ivUja-5q9E73ld44ezdeKuX-cGE",
+        "n": "0CXtOrsyIGkhhJ_sHzGbyK9U6sug4HdjdSNaq-FVbFFO_OeAaS8NvzM7DJXkZvmvZ7HNIPdlRk0-TCELmbOGK1RlddQZA_iic9DePydxloNJIWmUVI5GK1T84PxhjnMfBAD3SWPdTZ0zG1IubAjUJT4nwl0uVdzp0-LixbmKPQU87dqA1jt7ZuC73M55oZAyi1e2fzvgdxWyM7-NyvkZqwG2eGoDQ3SNb0rArlHTgdsLf1YsGPxn1wN3bSjhrq6af4fCnB5UVRb-r3g4NN_VJxBOc2xGDDoOgaPW9XW-BhSefc2hqRjTwtjaGiZFLdEuZdcq_mUB-AHc0YYD3_4VXw",
+    }
+
+    rsa_public_key_test = {
+        "keys": [
+            {
+                "kty": "RSA",
+                "e": "AQAB",
+                "use": "sig",
+                "kid": "8YN3pNDUUyho-tQB25afq8DKCrxt2V-bS6W9gRk0cgk",
+                "alg": "RS256",
+                "n": "0CXtOrsyIGkhhJ_sHzGbyK9U6sug4HdjdSNaq-FVbFFO_OeAaS8NvzM7DJXkZvmvZ7HNIPdlRk0-TCELmbOGK1RlddQZA_iic9DePydxloNJIWmUVI5GK1T84PxhjnMfBAD3SWPdTZ0zG1IubAjUJT4nwl0uVdzp0-LixbmKPQU87dqA1jt7ZuC73M55oZAyi1e2fzvgdxWyM7-NyvkZqwG2eGoDQ3SNb0rArlHTgdsLf1YsGPxn1wN3bSjhrq6af4fCnB5UVRb-r3g4NN_VJxBOc2xGDDoOgaPW9XW-BhSefc2hqRjTwtjaGiZFLdEuZdcq_mUB-AHc0YYD3_4VXw",
+            }
+        ]
+    }
+
     def get_test_app_client(self, app):
         app.testing = True
         return app.test_client()
@@ -104,15 +143,20 @@ class FlaskServerTestCase(unittest.TestCase):
 
     def get_token_header_value(self, profile_type):
         id_attribute = get_id_token_attribute_by_profile_type(profile_type)
+
         token_data = {
             id_attribute: self.get_user_id(profile_type),
             "aud": get_client_id(profile_type),
         }
+        key = jwt.api_jwk.PyJWK.from_dict(
+            self.rsa_private_key_test, algorithm="RS256"
+        ).key
 
         token = jwt.encode(
             token_data,
-            OIDC_CLIENT_SECRET,
-            algorithm="HS256",
+            key,
+            algorithm="RS256",
+            headers={"kid": self.rsa_private_key_test["kid"]},
         )
 
         return f"Bearer {token}"
@@ -126,7 +170,9 @@ class FlaskServerTestCase(unittest.TestCase):
         return headers
 
     def get_secure(self, location, profile_type=PROFILE_TYPE_PRIVATE, headers=None):
-        return self.client.get(
-            location,
-            headers=self.add_authorization_headers(profile_type, headers=headers),
-        )
+        with patch.object(jwt.PyJWKClient, "fetch_data") as fetch_data_mock:
+            fetch_data_mock.return_value = self.rsa_public_key_test
+            return self.client.get(
+                location,
+                headers=self.add_authorization_headers(profile_type, headers=headers),
+            )
