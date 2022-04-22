98fb4077babe6382f05a16c26d299e01591477a0
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2022-04-22 19:25:04 +0200

    Fix script

--- scripts/get_all.py
+++ scripts/get_all.py
@@ -6,7 +6,7 @@ from app import config, server, sisa_service
 
 bsn = argv[1]
 
-print("endpoint", config.SISA_API_ENDPOINT)
+print("endpoint", config.SISA_API_BSN_ENDPOINT)
 print("encryption key", config.SISA_ENCRYPTION_KEY)
 print("client id", config.SISA_CLIENT_ID)
 print("client secret", config.SISA_CLIENT_SECRET)
@@ -14,6 +14,6 @@ print("client secret", config.SISA_CLIENT_SECRET)
 with server.app.app_context():
     summary = sisa_service.get_all(bsn)
 
-print("\n\n\nResponse\n\n\n")
-json.dumps(summary, indent=4)
-print("\n\n\nend.Response\n\n\n")
+print("\n\n\nResponse")
+print(json.dumps(summary, indent=4))
+print("\nend.Response\n\n\n")
